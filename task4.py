e0a4bc9f950451b14481ea7f61f7d897b6c5b218
Bakkolla Hemanth kumar <bakkollahemanthkumar@gmail.com>
2025-06-26 18:22:59 +0530

    conversion oct to hex

---
+++ task4.py
@@ -0,0 +1,12 @@
+a=152455202562529
+b=int(a)
+print("decimal format is :",b)
+c=a
+d=bin(c)
+print("binary format is :",d)
+e=a
+f=oct(e)
+print("octal format is :",f)
+g=a
+h=hex(g)
+print("hexal format is :",h)
