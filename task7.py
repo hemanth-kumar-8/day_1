18434a8b573652a9804acd1c258680e4d9d3fe84
Bakkolla Hemanth kumar <bakkollahemanthkumar@gmail.com>
2025-06-26 18:42:21 +0530

    conversion bin to hex

---
+++ task7.py
@@ -0,0 +1,12 @@
+a=0x48bfa96c348
+b=int(a)
+print("decimal format is :",b)
+c=a
+d=bin(c)
+print("binary format is :",d)
+e=a
+f=oct(e)
+print("octal format is :",f)
+g=a
+h=hex(g)
+print("hexal format is :",h)
