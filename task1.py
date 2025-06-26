211f1a2a7b51c858bd3e3b202d74522608682449
Bakkolla Hemanth kumar <bakkollahemanthkumar@gmail.com>
2025-06-26 15:59:15 +0530

    converting int to hex

---
+++ task1.py
@@ -0,0 +1,12 @@
+a=78699762973
+b=bin(a)
+print("binary format is :",b)
+c=a
+d=oct(c)
+print("octal format is :",d)
+e=a
+f=hex(e)
+print("hexal format is :",f)
+g=a
+h=int(g)
+print("integer format is :",h)
